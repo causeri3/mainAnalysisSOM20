a1ddef7752e3bf9b4341730d5259ba604c1a6a9f
Causeri3 <vanessaca@web.de>
2021-01-12 11:48:46 +0100

    adding percentage output for fact sheets & small fixes

--- get_frequencies_means.R
+++ get_frequencies_means.R
@@ -35,6 +35,7 @@ vars=c('chronic_illness_hh_members',
        'home_learning',
        'health_household',
        'health_seek',
+       'humanitarian_assistance',
        'unvaccinated_why',
        'barriers_health',
        'cash_health',
@@ -92,6 +93,19 @@ vars=c('chronic_illness_hh_members',
        'deviceid',
        'consensus',
        'region',
+       'children_away_why_note',
+       'disp_current_note',
+       'displacement_note',
+       'district_idp',
+       'district_intended',
+       'eating_abnormally_length',
+       'education_level_note',
+       'health_issue_note',
+       'note_households_support',
+       'note_limitations',
+       'note_safety_affected',
+       'rooms_difference',
+       'shelther_rooms_confirm_note',
        other
        )
 
@@ -133,29 +147,29 @@ start<-5
 
 #####IDP-TABLE#####
 
-u_distr_idp<-unique(df$area_level[df$settlement_type=="IDP"])                                                          #unique list of areas which have idp-settlements  
+u_distr_idp<-unique(df$area_level[df$settlement_type=="IDP"])                                                           #unique list of areas which have idp-settlements  
 
-start_time <- Sys.time()                                                                                               #timer for fun
+start_time <- Sys.time()                                                                                                #timer for fun
 
-for (i in start:length(df)){                                                                                           #loop over all columns from "start" onward
-  for (k in 1:length(u_distr_idp)){                                                                                    #loop over all unique idp-districts
-    table_idp<-as.data.frame(prop.table(table(df[,i][df$area_level==u_distr_idp[k] & df$settlement_type=="IDP"])))     #compute frequencies, save as data frame
-    if (dim(table_idp)[1]==0){                                                                                         #solution in case there is no values:
-      colnames(table_idp)[1]<-"Var1"                                                                                   #name column as if there would be a value
-      table_idp[1,]<-NA                                                                                                #make value NA
-      table_idp$Freq<-NA                                                                                               #create second column as if there would be value and make value NA
+for (i in start:length(df)){                                                                                            #loop over all columns from "start" onward
+  for (k in 1:length(u_distr_idp)){                                                                                     #loop over all unique idp-districts
+    table_idp<-as.data.frame(prop.table(table(df[,i][df$area_level==u_distr_idp[k] & df$settlement_type=="IDP"])))      #compute frequencies, save as data frame
+    if (dim(table_idp)[1]==0){                                                                                          #solution in case there is no values:
+      colnames(table_idp)[1]<-"Var1"                                                                                    #name column as if there would be a value
+      table_idp[1,]<-NA                                                                                                 #make value NA
+      table_idp$Freq<-NA                                                                                                #create second column as if there would be value and make value NA
     }                                                                    
-    table_idp$var_names<- paste0(colnames(df[i]))                                                                      #add column with variable names
-    table_idp$settlement_type<-"IDP"                                                                                   #add column with info about settlement-type
-    table_idp$area<- u_distr_idp[k]                                                                                    #add column with area
-    if (i==start & k==1){long_table_idp<-table_idp}                                                                    #make initial long table from first data frame
-    else {long_table_idp<-rbind(long_table_idp,table_idp)}                                                             #add all data frames (from second run onward) row-wise onto first data frame
+    table_idp$var_names<- paste0(colnames(df[i]))                                                                       #add column with variable names
+    table_idp$settlement_type<-"IDP"                                                                                    #add column with info about settlement-type
+    table_idp$area<- u_distr_idp[k]                                                                                     #add column with area
+    if (i==start & k==1){long_table_idp<-table_idp}                                                                     #make initial long table from first data frame
+    else {long_table_idp<-rbind(long_table_idp,table_idp)}                                                              #add all data frames (from second run onward) row-wise onto first data frame
   }}
 
 
-end_time <- Sys.time()                                                                                                 #timer
-end_time - start_time                                                                                                  #timer
-
+end_time <- Sys.time()                                                                                                  #timer
+end_time - start_time                                                                                                   #timer
+#50 min
 
 #####NON-IDP-TABLE#####
 
@@ -180,7 +194,7 @@ for (i in start:length(df)){                                                    
 
 end_time <- Sys.time()                                                                                                  #timer
 end_time - start_time                                                                                                   #timer
-
+#1.1h
 
 #####IDP-TABLE [NUMERIC]#####
 
@@ -190,8 +204,8 @@ start_time <- Sys.time()                                                        
                                                                                                                          
   for (k in 1:length(u_distr_idp)){                                                                                     #loop over all areas
     df_num_idp<-df_num[df_num$area_level==u_distr_idp[k] & df_num$settlement_type=="IDP",]                              #subset with only survey with area and IDP                          
-    means<-data.frame(sapply(df_num_idp, function(x) mean(x, na.rm=T)))                                                 #compute means
-    table_num_idp <- cbind(variables=rownames(means), data.frame(means, row.names=NULL))                                #save variables and means as data frame
+    means_idp<-data.frame(sapply(df_num_idp[1:(length(df_num_idp)-2)], function(x) mean(x, na.rm=T)))                   #compute means
+    table_num_idp <- cbind(variables=rownames(means_idp), data.frame(means_idp, row.names=NULL))                        #save variables and means as data frame
     table_num_idp$settlement_type<-"IDP"                                                                                #add column with info about settlement-type
     table_num_idp$area<- u_distr_idp[k]                                                                                 #add column with area
     if (k==1){long_table_num_idp<-table_num_idp}                                                                        #make initial long table from first data frame
@@ -207,12 +221,12 @@ u_distr_host<-unique(df$area_level[df$settlement_type=="HC"])                   
 
 start_time <- Sys.time()                                                                                                #timer
 
-for (k in 1:length(u_distr_idp)){                                                                                       #loop over all areas
-  df_num_non_idp<-df_num[df_num$area_level==u_distr_idp[k] & df_num$settlement_type=="HC",]                             #subset with only survey with area and IDP                          
-  means<-data.frame(sapply(df_num_non_idp, function(x) mean(x, na.rm=T)))                                               #compute means
-  table_num_non_idp <- cbind(variables=rownames(means), data.frame(means, row.names=NULL))                              #save variables and means as data frame
+for (k in 1:length(u_distr_host)){                                                                                      #loop over all areas
+  df_num_non_idp<-df_num[df_num$area_level==u_distr_host[k] & df_num$settlement_type=="HC",]                            #subset with only survey with area and IDP                          
+  means_hc<-data.frame(sapply(df_num_non_idp[1:(length(df_num_non_idp)-2)], function(x) mean(x, na.rm=T)))              #compute means
+  table_num_non_idp <- cbind(variables=rownames(means_hc), data.frame(means_hc, row.names=NULL))                        #save variables and means as data frame
   table_num_non_idp$settlement_type<-"HC"                                                                               #add column with info about settlement-type
-  table_num_non_idp$area<- u_distr_idp[k]                                                                               #add column with area
+  table_num_non_idp$area<- u_distr_host[k]                                                                              #add column with area
   if (k==1){long_table_num_non_idp<-table_num_non_idp}                                                                  #make initial long table from first data frame
   else {long_table_num_non_idp<-rbind(long_table_num_non_idp,table_num_non_idp)}                                        #add all data frames (from second run onward) row-wise onto first data frame
 }
@@ -221,17 +235,21 @@ end_time - start_time                                                           
 
 
 #####JOIN TABLES AND FORMAT WIDE#####
-colnames(long_table_num_non_idp)[1:2]<-c("variables", "Freq")                                                           #fit column naming                  
-colnames(long_table_num_idp)[1:2]<-c("variables", "Freq")                                                               #fit column naming              
-long_table<-rbind(long_table_idp,long_table_non_idp)                                                                    #join tables row-wise
-long_table$variables <- paste0(long_table$var_names,"-", as.character(long_table$Var1))                                 #add identifying column for making table wide
-long<-long_table[,which(names(long_table)%in%c("area","settlement_type","variables","Freq"))]                           #get rid of not needed columns
-long<-rbind(long,long_table_num_non_idp,long_table_num_idp)                                                             #add means from numeric tables
-wide_table<-dcast(data = long, formula = area + settlement_type ~ variables, fun.aggregate = NULL, value.var = "Freq")  #convert to wide format
-index2<-which(grepl("-NA",names(wide_table)))                                                                           #get index of all columns containing "NA" in column name                                                                                                  #add the "_other" columns to the list of indexes which will be deleted          
-wide_table<-wide_table[-index2]                                                                                         #subset without NA columns     
+colnames(long_table_num_non_idp)[1:2]<-c("variables", "Freq")                                                                 #fit column naming                  
+colnames(long_table_num_idp)[1:2]<-c("variables", "Freq")                                                                     #fit column naming              
+long_table<-rbind(long_table_idp,long_table_non_idp)                                                                          #join tables row-wise
+long_table$variables <- paste0(long_table$var_names,"_", as.character(long_table$Var1))                                       #add identifying column for making table wide
+long<-long_table[,which(names(long_table)%in%c("area","settlement_type","variables","Freq"))]                                 #get rid of not needed columns                                                                
+long<-rbind(long,long_table_num_non_idp,long_table_num_idp)                                                                   #add means from numeric tables
+names(long)[which(names(long)=="area")]<-"area_level"                                                                         #fix in order to be able to afterwards only multiply the frequencies with 100
+wide_table<-dcast(data = long, formula = area_level + settlement_type ~ variables, fun.aggregate = NULL, value.var = "Freq")  #convert to wide format
+index2<-which(grepl("_NA",names(wide_table)))                                                                                 #get index of all columns containing "NA" in column name                                                                                                          
+wide_table<-wide_table[-index2]                                                                                               #subset without NA columns     
 
+#make frequencies to percentages 
+wide_table_perc<-wide_table
+wide_table_perc[,-which(names(wide_table_perc)%in%names(df_num))]<-wide_table_perc[,-which(names(wide_table_perc)%in%names(df_num))]*100
 
 #####EXPORT#####
-
+write.csv(wide_table_perc,"output/percentage_means_district_settlement.csv", row.names=FALSE)
 write.csv(wide_table,"output/frequency_means_district_settlement.csv", row.names=FALSE)
